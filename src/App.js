a1637c6b7d74625cd7c8744a1a7ef4e5e5f0276a
powerbarrusa <barr@covrigaru.com>
2019-12-31 20:39:46 -0700

    passing 'username' as props to output component'

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import UserInput from '../src/Components/UserInput'
 import UserOutput from '../src/Components/UserOutput'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <UserInput />
-        <UserOutput />
-      </div>
-    )
+  constructor(){
+    super()
+    this.state = {
+      username: "barry"
+    }
   }
-}
+  
+
+    render() {
+      return (
+        <div className="App">
+          <UserInput />
+          <UserOutput username = {this.state.username} />
+        </div>
+      )
+    }
+  }
+
+
 
 export default App
