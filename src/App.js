e7d8aee144fc18826a6384792552ecf70bb31af5
powerbarrusa <barr@covrigaru.com>
2020-01-06 13:29:51 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -10,18 +10,36 @@ class App extends Component {
       username: "barry"
     }
   }
-  
 
-    render() {
-      return (
-        <div className="App">
-          <UserInput />
-          <UserOutput username = {this.state.username} />
-        </div>
-      )
-    }
+  addNum = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+    console.log(this.state.username)
   }
 
+  consoleNum = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      username: e.target.value
+    })
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <UserInput
+          addNum={this.addNum}
+          consoleNum={this.consoleNum}
+          curVal={this.state.username}
+        />
+        <UserOutput
+          username = {this.state.username}
+          usrname = "Barr"
+        />
+      </div>
+    )
+  }
+}
 
 export default App
--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const UserInput = () => {
+const UserInput = (props) => {
   return (
-    <input label="Input Here"></input>
+    <div>
+      <label>Enter something here: </label>
+      <input
+        type="text"
+        value={props.curVal}
+        onChange={props.consoleNum}
+        onClick={() => console.log(props.username)}
+      />
+      <button onClick={() => console.log("clicked!")}>Click me!</button>
+    </div>
   )
 }
 
